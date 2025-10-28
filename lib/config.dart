c2d099d6399886b7de0287c7ecec07268144f890
poon <6631501059@lamduan.mfu.ac.th>
2025-10-28 21:46:39 +0700

    เปลี่ยนปุ่ม เพิ่มปุ่ม

--- lib/config.dart
+++ lib/config.dart
@@ -1,6 +1,9 @@
 
-//set ip in your own computer!!!
-//http://xxx.xxx.xx.x:3000
+// set ip in your own computer!!!
+// http://xxx.xxx.xx.x:3000
 class AppConfig {
   static const String baseUrl = 'http://192.168.80.1:3000';
 }
+// class AppConfig {
+//   static const String baseUrl = 'http://localhost:3000';
+// }
