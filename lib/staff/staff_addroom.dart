8b09474d9ca1eb3108fe754befdbdfd0ade6de1d
poon <6631501059@lamduan.mfu.ac.th>
2025-10-29 14:53:10 +0700

    เพิ่มหน้า add

---
+++ lib/staff/staff_addroom.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:roombooking/screens/home_screen.dart';
+
+class StaffAddroom extends StatefulWidget {
+  const StaffAddroom({super.key});
+
+  @override
+  State<StaffAddroom> createState() => _StaffAddroomState();
+}
+
+class _StaffAddroomState extends State<StaffAddroom> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
